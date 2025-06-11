78aa174e00e40e2bf753e7fa2cd9769362a6ea09
daniel.marques <daniel.marques@ivoryit.com.br>
2025-06-11 15:43:11 -0300

    feat: atualizando

---
+++ 01. INTRO/Cadastro.py
@@ -0,0 +1,21 @@
+cpf = input("Digite seu CPF?")
+
+cpf = cpf.strip()
+cpf = cpf.replace('.', '')
+cpf = cpf.replace('-', '')
+
+if len(cpf) == 11 and cpf.isnumeric:
+    print("CPF incorreto")
+else:
+    print(cpf)
+
+nome = input("Digite seu nome:")
+email = input("Digite seu email?")
+
+if nome and email:
+    pos_a = email.find("@")
+    servidor = email[pos_a:]
+    if pos_a != -1:
+        print('cadastro concluído!')
+else:
+    print('Digite seu nome e email corretamente')
